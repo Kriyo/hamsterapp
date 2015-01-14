965f8196dc7ae066bf387b7fca9273a544c1d6cc
Kriyo <krikenn@gmail.com>
2015-01-13 20:35:05 -0800

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Hamsterapp::Application.config.secret_key_base = 'ba4cc286b0b214796609a973a95518aa88ffba984e36b7497b8582cedec7bc779c82a3b6a490d1e910b9e42a1725500b24cdf6eba40127b2f7e50cbfb880282b'
