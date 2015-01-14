bcc05508b18f03d81bb0db4d7bc0f830ad8187c6
Kriyo <krikenn@gmail.com>
2015-01-13 20:38:48 -0800

    rails_apps_composer: add pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Hamsterapp::Application.routes.draw do
+  root to: 'visitors#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
